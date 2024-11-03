82ceeeb74170843a8a007a738a5635d655ab7fe7
Takeshi Yano <tyano.line@gmail.com>
2024-11-03 21:15:48 +0900

    - Add support for Azure AI (OpenAI) - Make externalization target JSON File

--- freeware_version_extractor.py
+++ freeware_version_extractor.py
@@ -1,67 +1,167 @@
 import time
+import json
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
 from bs4 import BeautifulSoup
+
 import anthropic
+from openai import AzureOpenAI
 
-def extract_version(driver, url, software_name):
+doller_to_en = 152
 
-    driver.get(url)
+demomode = False
+
+with open('models.json') as f:
+    models = json.load(f)
+
+input_tokens = 0
+output_tokens = 0
+
+def scraping(driver, url):
+    try:
+        driver.get(url)
+    except TimeoutException as e:
+        pass
+
     time.sleep(3)
 
     html = driver.page_source
 
-    # 取得したHTMLからBeautifulSoupオブフェクトを生成
-    # scriptやstyle及びその他タグの除去
+    # remove script and style tags
     soup = BeautifulSoup(html, "lxml")
     for s in soup(['script', 'style']):
         s.decompose()
 
     text = ' '.join(soup.stripped_strings)
 
+    return text
 
-    client = anthropic.Anthropic(api_key='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')
-    message = client.messages.create(
-        max_tokens=100,
-        system='以下はソフトウェア「' + software_name + '」の配布ページです。安定版の最新バージョンおよび、可能な場合はその提供日をJSON形式で出力してください。ベータ版やプレビュー版は除きます。キーは以下のとおりです。"version","release_date" 不明な場合はnullを格納してください。JSONのみ出力し余計な発言は不要です。',
-        model="claude-3-haiku-20240307",
-    #    model="claude-3-5-sonnet-latest",
-        messages=[
-            {"role": "user", "content": text},
-            {"role": "assistant", "content": '{\n "version":'}
-        ]
-    )
-    return ('{\n "version":' + message.content[0].text)
+def extract_version(model, software_name, text):
+    if demomode:
+        return ("0.0", 0, 0)
+    
+    system_text = \
+                f'This is the distribution page for the software"{software_name}".' \
+                f'Please output the latest stable version and, if available, its release date in JSON format.' \
+                'Exclude beta and preview versions(like "10.0-preview2" ). The keys are as follows: "version", "release_date"."' \
+                'Remove prefix like "v" or "ver" for version text."' \
+                'Date format is "yyyy/mm/dd".' \
+                'If version or release_date are unknown or not included in given text, store null.' \
+                'Reply JSON only(Do not include your comment).'
 
-targets = [
-    ("https://forest.watch.impress.co.jp/library/software/blackjmbdog/", "BlackJumboDog"),
-    ("https://github.com/lucasg/Dependencies", "Dependencies"),
-    ("https://jmeter.apache.org/download_jmeter.cgi", "Apache JMeter"),
-    ("https://sourceforge.net/p/cppcheck/news/", "CppCheck"),
-    ("https://forest.watch.impress.co.jp/library/software/crdiskinfo/", "CrystalDiskInfo"),
-    ("https://www.gimp.org/downloads/", "GIMP"),
-    ("https://marketplace.visualstudio.com/items?itemName=donjayamanne.githistory", "Git History"),
-    ("https://www.ffmpeg.org/download.html", "FFmpeg"),
-    ("https://github.com/icsharpcode/ILSpy/releases", "ILSpy"),
-    ("https://inkscape.org/release/inkscape-1.4/windows/64-bit/msi/?redirected=1", "InkSpace"),
-    ("https://a5m2.mmatsubara.com/", "A5:SQL Mk-2"),
-    ("https://teratermproject.github.io/", "Tera Term"),
-    ("https://www.roto21.net/husen/", "付箋紙21FE"),
-    ("https://www.selenium.dev/downloads/", "Selenium for Python"),
-    ('https://tortoisesvn.net/downloads.html', "tortoisesvn"),
+    with open(f'QueryText-{software_name}.txt', 'w', encoding='utf-8') as f:
+        f.write(system_text)
+        f.write('\n')
+        f.write(text)
+        f.close()
 
-]
+    useAzure = True
 
-driver = webdriver.Chrome()
+    resMessage = ''
+    input_tokens = 0
+    output_tokens = 0
 
-results = []
+    model_name = model['name']
+    api_key = model['apikey'],
+    if not 'gpt' in model_name.lower():
+        clude_client = anthropic.Anthropic(api_key=api_key)
+        message = clude_client.messages.create(
+            max_tokens=1000,
+            temperature=0.0,
+            system=system_text,
+            model=model_name,
+            messages=[
+                {"role": "user", "content": text},
+                {"role": "assistant", "content": '{\n "version":'}
+            ]
+        )
+        resMessage = ('{\n "version":' + message.content[0].text).replace('\n','')
+        input_tokens = message.usage.input_tokens
+        output_tokens = message.usage.output_tokens
+    else:
+        endpoint = model['endpoint']
+        azure_client = AzureOpenAI(
+            api_version="2023-05-15",
+            azure_endpoint = endpoint
+            )
 
-for (url, software_name) in targets:
-    version = extract_version(driver, url, software_name).replace('\n','')
-    print (software_name)
-    print (version)
-    results.append('{"softwarename": "' + software_name + '", "versioninfo": ' + version + '}\n')
+        response = azure_client.chat.completions.create(
+            model=model['name'],
+            temperature=0.0,
+            messages=[
+                {"role": "system", "content": system_text},
+                {"role": "user", "content": text},
+                {"role": "assistant", "content": '{\n "version":'}
+            ]
+        )
+        resMessage = (response.choices[0].message.content).replace('\n','')
+        input_tokens = response.usage.prompt_tokens
+        output_tokens = response.usage.completion_tokens
 
-driver.close()
+    return (
+        resMessage,
+        input_tokens,
+        output_tokens
+        )
 
-print ("{\n" + ",".join(results) + "}")
+if __name__ == '__main__':
+
+    with open('targets.json', encoding='utf-8') as f:
+        targets = json.load(f)
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--ignore-certificate-errors")
+
+    driver = webdriver.Chrome(options=options)
+
+    texts = []
+    results = []
+
+    driver.set_page_load_timeout(20)
+
+    start = time.time()
+
+    print ('Scraping..')
+    for target in targets:
+        software_name = target['software-name'].removeprefix("#")
+#        model_name = 'haiku' if not software_name.startswith('#') else 'sonnet'
+        model_name = 'gpt4omini'
+        software_name = software_name.removeprefix("#")
+        print (f'Scraping: {software_name}')
+        text = scraping(driver, target['url'])
+        texts.append((software_name, text, model_name))
+
+    driver.close()
+
+    total_cost_input = 0
+    total_cost_output = 0
+
+    start_extracting = time.time()
+
+    print ('-------------------------')
+    print ('Extracting version info by AI..')
+    for (software_name, text, model_name) in texts:
+        # Select AI Model
+        model = models[model_name]
+
+        print (software_name)
+        print (model["name"])
+
+        # Extract
+        (version, input_token, output_token) = extract_version(model, software_name, text)
+
+        print (version)
+        results.append(
+            f'{{"softwarename": "{software_name}", "text-len": {len(text)},"versioninfo": {version}}}\n')
+        total_cost_input = total_cost_input + (input_token  / 1000000.0 * model['pi']) * doller_to_en
+        total_cost_output = total_cost_output + (output_token / 1000000.0 * model['po']) * doller_to_en
+
+    end_time = time.time()
+
+    print ("{\n" + ",".join(results) + "}")
+
+    print (f" {total_cost_input+total_cost_output}(In={total_cost_input} + Out={total_cost_output}) yen ")
+    print (f" Elapsed time {end_time - start:.3f}sec ( {end_time - start_extracting:.3f}sec + {start_extracting - start:.3f}sec ) ")
